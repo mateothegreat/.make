6dd2a46cd1534138ba35bdc50e80164f4a0f7826
Matthew Davis <matthew@appsoa.io>
2018-02-17 14:22:16 +0000

    update help outputs

--- includes/help.mk
+++ includes/help.mk
@@ -2,19 +2,36 @@
 .DEFAULT_GOAL := help
 
 TIME		:= $(shell date +"%Y-%m-%d %H:%M:%S")
-GREEN  		:= $(shell tput -Txterm setaf 2)
-YELLOW 		:= $(shell tput -Txterm setaf 3)
-WHITE  		:= $(shell tput -Txterm setaf 7)
-RESET  		:= $(shell tput -Txterm sgr0)
+PURPLE 		:= $(shell tput setaf 129)
+GRAY  		:= $(shell tput setaf 245)
+GREEN  		:= $(shell tput setaf 34)
+BLUE 		:= $(shell tput setaf 25)
+YELLOW 		:= $(shell tput setaf 3)
+WHITE  		:= $(shell tput setaf 7)
+RESET  		:= $(shell tput sgr0)
+
 help:
 
-	@echo "\nUsage:\n\n  ${YELLOW}make${RESET} ${GREEN}<target>${RESET}\n\nTargets:\n"
+
+	@echo "${PURPLE}"
+	@echo "                                __                 __"
+	@echo "   __  ______  ____ ___  ____ _/ /____  ____  ____/ /"
+	@echo "  / / / / __ \/ __  __ \/ __  / __/ _ \/ __ \/ __  / "
+	@echo " / /_/ / /_/ / / / / / / /_/ / /_/  __/ /_/ / /_/ /  "
+	@echo " \__, /\____/_/ /_/ /_/\__,_/\__/\___/\____/\__,_/   "
+	@echo "/____                                                "
+	@echo "                       ${BLUE} yomateo.io, it ain't easy.   "
+	@echo "${RESET}"
+	@echo "Usage: ${YELLOW}make${RESET} ${GREEN}<target(s)>${RESET}"
+	@echo
+	@echo Targets:
+	@echo
 	@awk '/^[a-zA-Z\-\_0-9]+:/ { \
 		helpMessage = match(lastLine, /^## (.*)/); \
 		if (helpMessage) { \
 			helpCommand = substr($$1, 0, index($$1, ":")-1); \
 			helpMessage = substr(lastLine, RSTART + 3, RLENGTH); \
-			printf "  ${YELLOW}%-20s${RESET} ${GREEN}%s${RESET}\n", helpCommand, helpMessage; \
+			printf "  ${GREEN}%-20s${RESET} ${GRAY}%s${RESET}\n", helpCommand, helpMessage; \
 		} \
 	} \
 	{ lastLine = $$0 }' $(MAKEFILE_LIST)
