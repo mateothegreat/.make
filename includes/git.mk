95c15d59b81bb7bed40f3ec64559833152980f67
Matthew Davis <matthew@appsoa.io>
2018-02-17 15:28:07 +0000

    bump

--- includes/git.mk
+++ includes/git.mk
@@ -1,6 +1,10 @@
-update:
+
+## Update submodule(s) to HEAD from origin
+git/update:
 
 	# @git submodule update --remote
 	# git submodule foreach --recursive git checkout master
 
-	git submodule foreach 'git fetch origin; git checkout $(shell git rev-parse --abbrev-ref HEAD); git reset --hard origin/$(shell git rev-parse --abbrev-ref HEAD); git submodule update --recursive; gitclean -dfx'
+	REV := $(shell git rev-parse --abbrev-ref HEAD)
+
+	git submodule foreach 'git fetch origin; git checkout $(REV); git reset --hard origin/$(REV); git submodule update --recursive; git clean -dfx'
