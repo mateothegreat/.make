1d4dfdbde7530e21c4e0a62c4e7f0fa0846c7c6e
Matthew Davis <matthew@appsoa.io>
2018-02-17 15:33:21 +0000

    bump

--- includes/kubectl.mk
+++ includes/kubectl.mk
@@ -2,9 +2,6 @@ MANIFESTS 	:= $(shell find manifests -type f 2> /dev/null | tr '\r\n' ' ')
 
 .PHONY: manifests kube logs
 
-## Output manifests detected (used with make install, delete, get, describe, etc)
-manifests: ; @echo "MANIFESTS DETECTED: " $(MANIFESTS)
-
 ## Installs manifests to kubernetes using kubectl apply (make manifests to see what will be installed)
 install:	; @echo; for F in $(MANIFESTS); do echo -n "[ INSTALLING $$F ]: " | tr 'a-z' 'A-Z' ; envsubst < $$F | kubectl -n $$NS apply -f -; done; echo;
 
@@ -17,11 +14,14 @@ get:		; @echo; for F in $(MANIFESTS); do echo "\n[ RETRIEVING $$F ]: \n" | tr 'a
 ## Describes manifests to kubernetes using kubectl describe (make manifests to see what will be installed)
 describe:	; @echo; for F in $(MANIFESTS); do echo "\n[ DESCRIBING $$F ]: \n" | tr 'a-z' 'A-Z' ; envsubst < $$F | kubectl -n $$NS describe -f -; done; echo;
 
-## Find first pod and follow log output
-kube/logs: 	; kubectl --namespace $(NS) logs -f $(shell kubectl get pods --all-namespaces -lapp=$(APP) -o jsonpath='{.items[0].metadata.name}')
-
 ## Globally set the current-context (default namespace)
 context:    ; kubectl config set-context $(kubectl config current-context) --namespace=$(NS)
 
 ## Grab a shell in a running container
 shell:      ; kubectl exec $(shell kubectl get pods --all-namespaces -lapp=$(APP) -o jsonpath='{.items[0].metadata.name}') -it -- /bin/sh
+
+## Find first pod and follow log output
+dump/logs: 	; kubectl --namespace $(NS) logs -f $(shell kubectl get pods --all-namespaces -lapp=$(APP) -o jsonpath='{.items[0].metadata.name}')
+
+## Output manifests detected (used with make install, delete, get, describe, etc)
+dump/manifests: ; @echo "MANIFESTS DETECTED: " $(MANIFESTS)
