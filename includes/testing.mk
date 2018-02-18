381ef01f1412b59d5a1157002b0e75361ef3fa3a
Matthew Davis <matthew@appsoa.io>
2018-02-18 16:33:09 -0600

    basic auth for curl tests

--- includes/testing.mk
+++ includes/testing.mk
@@ -1,10 +1,10 @@
 
 
 ### Try to curl http://$(HOST)
-testing/curlhttp:	guard-HOST	; curl -vv http://$(HOST)
+testing/curlhttp:	guard-HOST	; curl -vv http://$(HOST) -u user:pass
 
 ### Try to curl https://$(HOST)
-testing/curlhttps:	guard-HOST	; curl -vv http://$(HOST)
+testing/curlhttps:	guard-HOST	; curl -vv http://$(HOST) -u user:pass
 
 ### Retrieve external IP from api.ipify.org
 testing/getip: 					; curl https://api.ipify.org
