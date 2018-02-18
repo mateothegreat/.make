5ef5dd4229c6e92b466438b4edea4f7cab9ce3ab
Matthew Davis <matthew@appsoa.io>
2018-02-18 17:23:01 -0600

    bump

--- includes/testing.mk
+++ includes/testing.mk
@@ -1,6 +1,6 @@
 
 ### Try to curl http & https from $(HOST)
-testing/curl: testing/curlhttp testing/curlhttps
+testing-curl: testing/curlhttp testing/curlhttps
 
 ### Try to curl http://$(HOST)
 testing/curlhttp:	guard-HOST	; @echo -n "${BLUE}Testing HTTP Response Code for${GREEN} http://$(HOST):${RESET} "; if [ "$(shell curl -o -I -L -s -w "%{http_code}\n" https://$(HOST) -u user:pass)" = "200" ]; then echo "${YELLOW}OK${RESET}"; else echo "${YELLOW}ERROR, non 200 reponse code${RESET}"; fi 
