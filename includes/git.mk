ee5474813aec98f40f26f2cdecaa95f455d6a511
Matthew Davis <matthew@appsoa.io>
2018-02-19 09:35:19 -0600

    bump

--- includes/git.mk
+++ includes/git.mk
@@ -3,3 +3,8 @@
 git/update:
 
 	git submodule foreach 'git fetch origin; git checkout master; git reset --hard origin/master; git submodule update --recursive; git clean -dfx'
+
+### Pull submodules to latest commits instead of what repos point to
+git/latest
+
+	git submodule update --init --recursive
