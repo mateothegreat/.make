11ba918c6266add1452b0bf9d633f3331cdcb5b5
Matthew Davis <matthew@appsoa.io>
2018-02-21 04:01:15 -0800

    bump

--- includes/help.mk
+++ includes/help.mk
@@ -27,3 +27,16 @@ help:
 	} \
 	{ lastLine = $$0 }' $(MAKEFILE_LIST)
 	@echo
+	@echo
+	@echo Tools:
+	@echo
+	@awk '/^[a-zA-Z\/\-\_0-9]+:/ { \
+		helpMessage = match(lastLine, /^### (.*)/); \
+		if (helpMessage) { \
+			helpCommand = substr($$1, 0, index($$1, ":")-1); \
+			helpMessage = substr(lastLine, RSTART + 3, RLENGTH); \
+			printf "  ${GREEN}%-20s${RESET} ${GRAY}%s${RESET}\n", helpCommand, helpMessage; \
+		} \
+	} \
+	{ lastLine = $$0 }' $(MAKEFILE_LIST)
+	@echo
--- includes/testing.mk
+++ includes/testing.mk
@@ -9,4 +9,4 @@ testing/curlhttp:	guard-HOST	; @echo -n "${BLUE}Testing HTTP Response Code for${
 testing/curlhttps:	guard-HOST	; @echo -n "${BLUE}Testing HTTP Response Code for${GREEN} https://$(HOST):${RESET} "; if [ "$(shell curl -o -I -L -s -w "%{http_code}\n" https://$(HOST) -u user:pass)" = "200" ]; then echo "${YELLOW}OK${RESET}"; else echo "${YELLOW}ERROR, non 200 reponse code${RESET}"; fi 
 
 ### Retrieve external IP from api.ipify.org
-testing/getip: 					; curl https://api.ipify.org
+testing/getip: 					; curl https://api.ipify.org && echo
